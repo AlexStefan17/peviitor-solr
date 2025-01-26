0c3de8510e8197d8691a165a98ea255de36f820b
Alex Stefan <stefan.alex170299@gmail.com>
2025-01-26 23:45:23 +0200

    added schema

--- solr-cores.sh
+++ solr-cores.sh
@@ -15,3 +15,6 @@ echo "Creating Solr cores"
 docker exec -it $CONTAINER_NAME bin/solr create_core -c $CORE_NAME
 docker exec -it $CONTAINER_NAME bin/solr create_core -c $CORE_NAME_2
 docker exec -it $CONTAINER_NAME bin/solr create_core -c $CORE_NAME_3
+
+docker cp managed-schema.xml solr-container:/var/solr/data/$CORE_NAME_2/conf/managed-schema
+docker cp managed-schema solr-container:/var/solr/data/$CORE_NAME_2/conf/managed-schema
