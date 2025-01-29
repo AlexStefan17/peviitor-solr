63f5248fd18b9b971d38e7e9c3bc5c8b7769772e
Alex Stefan <stefan.alex170299@gmail.com>
2025-01-29 21:21:21 +0200

    added add-field and add-copy-field

--- solr-cores.sh
+++ solr-cores.sh
@@ -16,5 +16,189 @@ docker exec -it $CONTAINER_NAME bin/solr create_core -c $CORE_NAME
 docker exec -it $CONTAINER_NAME bin/solr create_core -c $CORE_NAME_2
 docker exec -it $CONTAINER_NAME bin/solr create_core -c $CORE_NAME_3
 
-docker cp managed-schema.xml solr-container:/var/solr/data/$CORE_NAME_2/conf/managed-schema
-docker cp managed-schema solr-container:/var/solr/data/$CORE_NAME_2/conf/managed-schema
+##### CORE Jobs ####
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-field": [
+      {
+        "name": "job_link",
+        "type": "text_general",
+        "stored": true,
+        "indexed": true
+        "multiValued": true
+        "uninvertible": true
+      }
+    ]
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-field": [
+      {
+        "name": "job_title",
+        "type": "text_general",
+        "stored": true,
+        "indexed": true
+        "multiValued": true
+        "uninvertible": true
+      }
+    ]
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-field": [
+      {
+        "name": "company",
+        "type": "text_general",
+        "stored": true,
+        "indexed": true
+        "multiValued": true
+        "uninvertible": true
+      }
+    ]
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-field": [
+      {
+        "name": "company_str",
+        "type": "string",
+        "stored": true,
+        "indexed": true,
+        "docValues": true,
+        "uninvertible": true,
+        "omitNorms": true,
+        "omitTermFreqAndPositions": true,
+        "sortMissingLast": true,
+      }
+    ]
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-field": [
+      {
+        "name": "hiringOrganization.name",
+        "type": "text_general",
+        "stored": true,
+        "indexed": true
+        "multiValued": true
+        "uninvertible": true
+      }
+    ]
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-field": [
+      {
+        "name": "country",
+        "type": "text_general",
+        "stored": true,
+        "indexed": true
+        "multiValued": true
+        "uninvertible": true
+      }
+    ]
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-field": [
+      {
+        "name": "city",
+        "type": "text_general",
+        "stored": true,
+        "indexed": true
+        "multiValued": true
+        "uninvertible": true
+      }
+    ]
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-field": [
+      {
+        "name": "county",
+        "type": "text_general",
+        "stored": true,
+        "indexed": true
+        "multiValued": true
+        "uninvertible": true
+      }
+    ]
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-copy-field": {
+      "source": "job_link",
+      "dest": "_text_"
+    }
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-copy-field": {
+      "source": "job_title",
+      "dest": "_text_"
+    }
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-copy-field": {
+      "source": "company",
+      "dest": ["_text_", "company_str", "hiringOrganization.name"]
+    }
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-copy-field": {
+      "source": "hiringOrganization.name",
+      "dest": "hiringOrganization.name_str"
+    }
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-copy-field": {
+      "source": "country",
+      "dest": "_text_"
+    }
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-copy-field": {
+      "source": "city",
+      "dest": "_text_"
+    }
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+
+##### CORE Logo ####
+
+docker exec -it solr-container curl -X POST -H "Content-Type: application/json" \
+  --data '{
+    "add-field": [
+      {
+        "name": "url",
+        "type": "text_general",
+        "stored": true,
+        "indexed": true
+        "multiValued": true
+        "uninvertible": true
+      }
+    ]
+  }' http://localhost:8983/solr/$CORE_NAME_2/schema
+
+# docker cp managed-schema.xml solr-container:/var/solr/data/$CORE_NAME_2/conf/managed-schema
+# docker cp managed-schema solr-container:/var/solr/data/$CORE_NAME_2/conf/managed-schema
